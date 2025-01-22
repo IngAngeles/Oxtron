6a40d6f771fbea3d7017d09b6a7b2be19519d0f1
AndyZam99 <77559233+AndyZam99@users.noreply.github.com>
2025-01-22 11:12:04 -0600

    Update traductor

--- components/lang/Header.tsx
+++ components/lang/Header.tsx
@@ -1,22 +1,22 @@
-import Link from 'next/link';
+//import Link from 'next/link';
 import { Locale } from '@/i18n.config';
 import { getDictionary } from '@/lib/dictionary';
 import LocalSwitcher from './locale-switcher';
 
 export default async function Header({ lang }: { lang: Locale }) {
     const dictionary = await getDictionary(lang);
-    const dashboardTitle = dictionary.pages.dashboard.title;
+    ///const dashboardTitle = dictionary.pages.dashboard.title;
 
     return (
         <header className='py-6'>
             <nav className='container flex items-center justify-between'>
                 <ul className='flex gap-x-8'>
-                    <li>
+                    {/* <li>
                         <Link href={`/${lang}`}>{dashboardTitle}</Link>
                     </li>
                     <li>
                         <Link href={`/${lang}/dashboard`}>{dashboardTitle}</Link>
-                    </li>
+                    </li> */}
                 </ul>
                 <LocalSwitcher />
             </nav>
