6be92607d47a9690ae3ea72b2ad794066bb93891
fernandevx <ortizfernando05@gmail.com>
2025-02-04 15:46:16 -0600

    feat: add type of equipment interface

--- constants/types.ts
+++ constants/types.ts
@@ -399,3 +399,9 @@ export interface BasicResponse<T> {
     success: boolean;
     data: T;
 }
+
+export interface TypeOfEquipment {
+    idManufacturingCboEquipment: number;
+    description: string;
+    active: number;
+}
